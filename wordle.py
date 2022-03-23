507682993e9d5f48d81a4c55086f16f1ff70da1d
rijulm <mahajanrijul@gmail.com>
2022-03-23 16:28:00 -0500

    hi

--- wordle.py
+++ wordle.py
@@ -4,3 +4,9 @@
 import urllib.request
 import re
 
+# here's a list of all the possible words: https://github.com/tabatkins/wordle-list/blob/main/words
+
+# should be trivial to download, extract, and sort.
+# perhaps sort and then rewrite to the file to avoid doing it every time it is run?
+# hi donna
+
